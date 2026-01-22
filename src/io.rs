67437996cde20f6988fd47f7f36464434e591bca
Lee ByeongJun <lbj199874@gmail.com>
2026-01-22 09:25:53 +0900

    pipeline reader

--- src/io.rs
+++ src/io.rs
@@ -1,4 +1,5 @@
 use std::{env, fs, io, path::PathBuf};
+use std::io::Read;
 
 pub fn read_file(path: &PathBuf) -> io::Result<String> {
     fs::read_to_string(path)
@@ -22,4 +23,10 @@ fn get_editor() -> String {
     env::var("EDITOR")
         .or_else(|_| env::var("VISUAL"))
         .unwrap_or_else(|_| "vim".to_string())
+}
+
+fn read_stdin() -> io::Result<String> {
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer)?;
+    Ok(buffer)
 }
